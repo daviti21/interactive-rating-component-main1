6eba9c36332bd807eae80acc1ff430f89139afb1
Davit Modebadze <dmodebadze63@gmail.com>
2025-11-20 20:58:18 +0400

    Initial commit

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const four = document.querySelector('#four');
 const five = document.querySelector('#five');
 const button = document.querySelector('#submit');
-
+const selectedBox1 = document.querySelector('#selected-box1');
+const selectedBox2 = document.querySelector('#selected-box2');
 
  four.addEventListener('click', () => {
     four.classList.toggle('active');
    if(four.classList.contains('active')){
    five.classList.add('not-active');
+   selectedBox2.style.display = 'none';
  }else{
    five.classList.remove('not-active');
+    selectedBox2.style.display = 'block';
  }
  })
 
@@ -16,8 +19,10 @@ const button = document.querySelector('#submit');
    five.classList.toggle('active');
    if(five.classList.contains('active')){
    four.classList.add('not-active');
+    selectedBox1.style.display = 'none';
  }else{
    four.classList.remove('not-active');
+    selectedBox1.style.display = 'block';
  }
  })
  
