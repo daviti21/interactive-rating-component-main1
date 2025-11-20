52a2ec7f00bc8e81f5de92b64aa39d0cf76a0645
Davit Modebadze <dmodebadze63@gmail.com>
2025-11-20 16:39:22 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const button = document.querySelector('#submit');
+
+
+ four.addEventListener('click', () => {
+    four.classList.toggle('active');
+  
+ })
+
+ five.addEventListener('click', () => {
+   five.classList.toggle('active');
+ })
+ 
+  
+button.addEventListener('click', () => {
+   if(four.classList.contains('active') || five.classList.contains('active')){
+      window.location.href = 'active.html';
+   }
+})
+ 
+ 
